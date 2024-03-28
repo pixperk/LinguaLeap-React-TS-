f208f4ee4f53b659621a95fb7c20b49aec64a86c
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2024-03-28 11:34:49 +0530

    text-to-speech

--- src/components/Learning.tsx
+++ src/components/Learning.tsx
@@ -1,8 +1,8 @@
 import { Button, Container, Stack, Typography } from "@mui/material"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useNavigate, useSearchParams } from "react-router-dom"
 import { ArrowBack, NavigateNext, VoiceChat } from "@mui/icons-material";
-import { translateWords } from "../utils/features";
+import { fetchAudio, translateWords } from "../utils/features";
 import { useDispatch, useSelector } from "react-redux";
 import { clearState, getWordsFail, getWordsRequest, getWordsSuccess } from "../redux/slices";
 import Loader from "./Loader";
@@ -10,14 +10,28 @@ import Loader from "./Loader";
 
 const Learning = () => {
   const [count, setCount] = useState<number>(0)
+  const[audioSrc, setAudioSrc] = useState<string>("")
+  const audioRef = useRef(null)
   const params = useSearchParams()[0].get("language") as LangType
   const navigate= useNavigate()
   const dispatch = useDispatch()
 
   const {loading,error,words} = useSelector((state:{root:StateType})=>state.root)
 
+  const audioHandler = async () => {
+    const player: HTMLAudioElement = audioRef.current!;
+
+    if (player) {
+      player.play();
+    } else {
+      const data = await fetchAudio(words[count]?.word, params);
+      setAudioSrc(data);
+    }
+  };
+
   const nextHandler = ():void=>{
     setCount((prev)=>prev+1)
+    setAudioSrc("")
   }
 
   useEffect(() => {
@@ -45,6 +59,9 @@ const Learning = () => {
     padding:"1rem"
   }}> 
 
+  {audioSrc && <audio src={audioSrc} autoPlay ref={audioRef}></audio>}
+
+
   <Button onClick={count===0?()=>navigate("/"):()=>setCount((prev)=>prev-1)}>
     <ArrowBack/>
   </Button>
@@ -54,13 +71,14 @@ const Learning = () => {
       {count+1}-{words[count]?.word}
     </Typography>
     <Typography color={"#f72585"} variant="h4">
-      :{words[count]?.meaning}
+      : {words[count]?.meaning}
     </Typography>
     <Button sx={
       {
         borderRadius:"50%",
       }
-    }>
+    }
+    onClick={audioHandler}>
       <VoiceChat/>
     </Button>
   </Stack>
@@ -70,7 +88,7 @@ const Learning = () => {
   }} variant="contained"
   onClick={count===words.length-1?()=>navigate("/quiz"):nextHandler} >
     {
-      count===words.length-1?"Text":
+      count===words.length-1?"Move to Test":
       <NavigateNext/>
     }
     
--- src/utils/features.ts
+++ src/utils/features.ts
@@ -2,64 +2,110 @@ import axios from "axios";
 import { generate } from "random-words";
 import _ from "lodash";
 
-const generateOptions = (meaning:{
-    Text:string
-}[],index:number): string[]=>{
+const generateOptions = (
+  meaning: {
+    Text: string;
+  }[],
+  index: number
+): string[] => {
+  const correctAns: string = meaning[index].Text;
+  const exceptCorrect = meaning.filter((i) => i.Text !== correctAns);
+  const incorrectOptions: string[] = _.sampleSize(exceptCorrect, 3).map(
+    (i) => i.Text
+  );
 
-const correctAns:string = meaning[index].Text
-const exceptCorrect = meaning.filter(i=>i.Text!==correctAns)
-const incorrectOptions:string[] = _.sampleSize(exceptCorrect,3).map((i)=>i.Text)
+  const mcqOptions = _.shuffle([...incorrectOptions, correctAns]);
+  return mcqOptions;
+};
 
-const mcqOptions = _.shuffle([...incorrectOptions,correctAns])
-    return mcqOptions
-}
+export const translateWords = async (params: LangType): Promise<WordType[]> => {
+  try {
+    const words = (generate(10) as string[]).map((i) => ({
+      Text: i,
+    }));
 
-export const translateWords = async(params:LangType):Promise<WordType[]> =>{
-    try {
-        const words = (generate(8) as string[]).map((i) => ({
-            Text: i
-        }));
+    const response = await axios.post(
+      "https://microsoft-translator-text.p.rapidapi.com/translate",
+      words,
+      {
+        params: {
+          "to[0]": params,
+          "api-version": "3.0",
+          profanityAction: "NoAction",
+          textType: "plain",
+        },
+        headers: {
+          "content-type": "application/json",
+          "X-RapidAPI-Key":
+            "a218ab7afcmsh31bf0fb0fa5b711p1236e1jsnb62717a2ac9b",
+          "X-RapidAPI-Host": "microsoft-translator-text.p.rapidapi.com",
+        },
+      }
+    );
+    const received: FetchedDataType[] = response.data;
+    const arr: WordType[] = received.map((i, index) => {
+      const options: string[] = generateOptions(words, index);
 
-        const response=await axios.post('https://microsoft-translator-text.p.rapidapi.com/translate',words,{
-            params: {
-                'to[0]': params,
-                'api-version': '3.0',
-                profanityAction: 'NoAction',
-                textType: 'plain'
-              },
-              headers: {
-                'content-type': 'application/json',
-                'X-RapidAPI-Key': 'a218ab7afcmsh31bf0fb0fa5b711p1236e1jsnb62717a2ac9b',
-                'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com'
-              },
-        })
-        const received:FetchedDataType[] = response.data
-        const arr:WordType[] = received.map((i,index)=>{
+      return {
+        word: i.translations[0].text,
+        meaning: words[index].Text,
+        options,
+      };
+    });
 
-            const options:string[]=generateOptions(words, index)
+    return arr;
+  } catch (error) {
+    console.log(error);
+    throw new Error("error");
+  }
+};
 
-            return{
-                word:i.translations[0].text,
-                meaning:words[index].Text,
-                options
-            }
-        })
-      
-        return arr
-        
-    } catch (error) {
-        console.log(error)
-        throw new Error("error")
-    }
-}
+export const countMatchingElements = (
+  arr1: string[],
+  arr2: string[]
+): number => {
+  if (arr1.length !== arr2.length) throw new Error("Arrays not equal");
 
-export const countMatchingElements = (arr1:string[],arr2:string[]):number =>{
-    if(arr1.length!==arr2.length) throw new Error("Arrays not equal")
+  let matchingCount = 0;
+  for (let index = 0; index < arr1.length; index++) {
+    if (arr1[index] === arr2[index]) matchingCount++;
+  }
 
-    let matchingCount = 0
-    for (let index = 0; index < arr1.length; index++) {
-       if(arr1[index]===arr2[index]) matchingCount++
-    }
+  return matchingCount;
+};
 
-    return matchingCount
-}
+export const fetchAudio = async (
+    text: string,
+    language: LangType
+  ): Promise<string> => {
+    const key = import.meta.env.VITE_TEXT_TO_SPEECH_API;
+    const rapidKey = import.meta.env.VITE_RAPID_API;
+  
+    const encodedParams = new URLSearchParams({
+      src: text,
+      r: "0",
+      c: "mp3",
+      f: "8khz_8bit_mono",
+      b64: "true",
+    });
+  
+    if (language === "ja") encodedParams.set("hl", "ja-jp");
+    else if (language === "es") encodedParams.set("hl", "es-es");
+    else if (language === "fr") encodedParams.set("hl", "fr-fr");
+    else encodedParams.set("hl", "hi-in");
+  
+    const { data }: { data: string } = await axios.post(
+      "https://voicerss-text-to-speech.p.rapidapi.com/",
+      encodedParams,
+      {
+        params: { key },
+        headers: {
+          "content-type": "application/x-www-form-urlencoded",
+          "X-RapidAPI-Key": rapidKey,
+          "X-RapidAPI-Host": "voicerss-text-to-speech.p.rapidapi.com",
+        },
+      }
+    );
+  
+    return data;
+  };
